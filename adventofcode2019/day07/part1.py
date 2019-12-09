de14a0957599b6d8c179dad184557e2ec9889de9
wlockiv <walker.b.lockard@gmail.com>
2019-12-08 18:50:52 -0600

    day 07 solution

--- adventofcode2019/day07/part1.py
+++ adventofcode2019/day07/part1.py
@@ -8,21 +8,68 @@ class Day07P1(IntcodeComputer):
     def __init__(self, input_file='input', test_input=None):
         super(Day07P1, self).__init__(input_file, test_input=test_input)
 
-    def run_amplifiers(self, data, sequence):
-        input_signal = 0
-        for code in sequence:
-            self.run_diagnostic(data, code)
+    @staticmethod
+    def run_diagnostic(data, inputs):
+        start = 0
+        end = 4 if Day07P1.parse_opcode(data[0])[0] in (1, 2) else 2
+        test_results = []
+
+        while True:
+            current_slice = data[start:end]
+            opc, mode_1, mode_2 = Day07P1.parse_opcode(current_slice[0])
+            if opc == 99:
+                break
+
+            if opc in (1, 2, 7, 8):
+                val_1 = current_slice[1] if mode_1 else data[current_slice[1]]
+                val_2 = current_slice[2] if mode_2 else data[current_slice[2]]
+                out_pos = current_slice[3]
+                if opc == 1:
+                    data[out_pos] = val_1 + val_2
+                elif opc == 2:
+                    data[out_pos] = val_1 * val_2
+                elif opc == 7:
+                    data[out_pos] = 1 if val_1 < val_2 else 0
+                elif opc == 8:
+                    data[out_pos] = 1 if val_1 == val_2 else 0
+                start += 4
+            elif opc in (3, 4):
+                out_pos = current_slice[1]
+                if opc == 3:
+                    if len(inputs) == 0:
+                        raise Exception('No input!')
+                    data[out_pos] = inputs.pop(0)
+                elif opc == 4:
+                    test_results.append(data[out_pos])
+                start += 2
+            elif opc in (5, 6):
+                val_1 = current_slice[1] if mode_1 else data[current_slice[1]]
+                val_2 = current_slice[2] if mode_2 else data[current_slice[2]]
+                if opc == 5 and val_1:
+                    start = val_2
+                elif opc == 6 and not val_1:
+                    start = val_2
+                else:
+                    start += 3
+
+            if (next_opc := Day07P1.parse_opcode(data[start])[0]) in (1, 2, 7, 8):
+                end = start + 4
+            elif next_opc in (3, 4):
+                end = start + 20
+            elif next_opc in (5, 6):
+                end = start + 3
+        return test_results
 
     @property
     def solution(self, input_signal=0):
-        output_signals = []
-        for perm in itertools.permutations(range(5)):
+        max_thruster_signal = 0
+        phase_perms = [list(perm) for perm in itertools.permutations(range(5))]
+        for perm in phase_perms:
             output = 0
-            for code in perm:
-                output += sum([self.amplify(self.parsed_input, code)])
-                output_signals.append(output)
-
-        return max(output_signals)
+            while len(perm) > 0:
+                output = self.run_diagnostic(self.parsed_input.copy(), [perm.pop(0), output])[-1]
+            max_thruster_signal = max(max_thruster_signal, output)
+        return max_thruster_signal
 
 
 def solve(test_input=None):
--- adventofcode2019/day07/part2.py
+++ adventofcode2019/day07/part2.py
@@ -1,46 +1,107 @@
 import math
 import os
+import itertools
 
-from adventofcode2019.day01.part1 import Module as P1Module
-from adventofcode2019.solution import Solution
+from adventofcode2019.day07.part1 import Day07P1
 
 
-class Day01P2(Solution):
+class Day07P2(Day07P1):
     def __init__(self, input_file='input', test_input=None):
-        super(Day01P2, self).__init__(input_file, test_input=test_input)
-        self.mass_list = [Module(mass) for mass in self.parsed_input]
-
-    @property
-    def solution(self):
-        # return sum([mod.fuel_needed() for mod in self.mass_list])
-        return sum([mod.fuel_needed() for mod in self.mass_list])
-
-
-class Module(P1Module):
-    def __init__(self, mass):
-        super(Module, self).__init__(mass)
+        super(Day07P2, self).__init__(input_file, test_input=test_input)
 
     @staticmethod
-    def fuel_formula(fuel_or_mass):
-        return math.floor(fuel_or_mass / 3) - 2
+    def run_diagnostic_feedback(data, inputs, pointer=0):
+        start = pointer
+        if Day07P1.parse_opcode(data[0])[0] in (1, 2):
+            end = start + 4
+        else:
+            end = start + 2
 
-    def fuel_needed(self):
-        result = [self.fuel_formula(self.mass)]
-        while True:
-            requirement = self.fuel_formula(result[-1])
+        test_results = []
 
-            if requirement > 0:
-                result.append(requirement)
-            else:
-                break
-        return sum(result)
+        while start < len(data):
+            current_slice = data[start:end]
+            opc, mode_1, mode_2 = Day07P1.parse_opcode(current_slice[0])
+            if opc == 99 and len(test_results):
+                return test_results[-1], None
+            elif opc == 99 and not len(test_results):
+                return 0, None
+
+            if opc in (1, 2, 7, 8):
+                val_1 = current_slice[1] if mode_1 else data[current_slice[1]]
+                val_2 = current_slice[2] if mode_2 else data[current_slice[2]]
+                out_pos = current_slice[3]
+                if opc == 1:
+                    data[out_pos] = val_1 + val_2
+                elif opc == 2:
+                    data[out_pos] = val_1 * val_2
+                elif opc == 7:
+                    data[out_pos] = 1 if val_1 < val_2 else 0
+                elif opc == 8:
+                    data[out_pos] = 1 if val_1 == val_2 else 0
+                start += 4
+            elif opc in (3, 4):
+                out_pos = current_slice[1]
+                if opc == 3:
+                    if len(inputs) == 0:
+                        raise Exception('No input!')
+                    data[out_pos] = inputs.pop(0)
+                    start += 2
+                elif opc == 4:
+                    test_results.append(data[out_pos])
+                    start += 2
+                    return test_results[-1], start
+            elif opc in (5, 6):
+                val_1 = current_slice[1] if mode_1 else data[current_slice[1]]
+                val_2 = current_slice[2] if mode_2 else data[current_slice[2]]
+                if opc == 5 and val_1:
+                    start = val_2
+                elif opc == 6 and not val_1:
+                    start = val_2
+                else:
+                    start += 3
+
+            if (next_opc := Day07P1.parse_opcode(data[start])[0]) in (1, 2, 7, 8):
+                end = start + 4
+            elif next_opc in (3, 4):
+                end = start + 20
+            elif next_opc in (5, 6):
+                end = start + 3
+        # return test_results[-1], None
+
+    @staticmethod
+    def run_with_feedback(data, phase_setting):
+        programs, pointers, inputs = [], [], []
+        num_amps = len(phase_setting)
+        amp_output = 0
+
+        for i in range(0, num_amps):
+            programs.append(data.copy())
+            pointers.append(0)
+            inputs.append([phase_setting[i]])
+
+        while pointers[0] is not None:
+            for i in range(0, num_amps):
+                inputs[i].append(amp_output)
+                amp_output, pointer = Day07P2.run_diagnostic_feedback(programs[i], inputs[i], pointers[i])
+                pointers[i] = pointer
+        return inputs[0][0]
+
+    @property
+    def solution(self, input_signal=0):
+        max_thruster_signal = 0
+        phase_perms = [list(perm) for perm in itertools.permutations(range(5, 10))]
+        for perm in phase_perms:
+            output = self.run_with_feedback(self.parsed_input.copy(), perm)
+            max_thruster_signal = max(max_thruster_signal, output)
+        return max_thruster_signal
 
 
 def solve(test_input=None):
     location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     input_file = os.path.join(location, 'input')
 
-    return Day01P2(input_file, test_input).solution
+    return Day07P2(input_file, test_input).solution
 
 
 if __name__ == '__main__':
